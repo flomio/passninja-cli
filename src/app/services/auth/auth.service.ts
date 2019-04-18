e18e7305992d1c4be1a301b941868de8273962f4
Matthew Keil <malibuss67@gmail.com>
2019-04-18 19:52:03 -0400

    auth.service complete, need to test

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,16 +1,24 @@
 import { Inject } from "@angular/core"
 import { BehaviorSubject } from "rxjs"
 import { filter, take } from "rxjs/operators"
-import * as AWS from "aws-sdk"
-import * as util from "util"
+import { promisify } from "util"
 
 import {} from "../../logging"
 import { CONFIG_TOKEN } from "../../injection-tokens"
 
 import { normalizedCredentials } from "./util"
 
+import {
+  config as CONFIG,
+  Iot,
+  CognitoIdentityCredentials,
+  CognitoIdentityServiceProvider
+} from "aws-sdk"
+
 export class AuthService {
-  credentialsSubject: BehaviorSubject<any>
+  credentialsSubject: BehaviorSubject<
+    CognitoIdentityCredentials | false
+  > = new BehaviorSubject(false)
 
   signedRequest: (
     url: string,
@@ -22,8 +30,7 @@ export class AuthService {
 
   constructor(@Inject(CONFIG_TOKEN) private options: any) {
     // TODO: should this not be set somewhere else? Probably in the config.ts
-    AWS.config.region = this.options.awsResources.region
-    this.credentialsSubject = new BehaviorSubject(null)
+    CONFIG.region = this.options.awsResources.region
     this.configureSignedRequest()
   }
 
@@ -51,84 +58,84 @@ export class AuthService {
     }
   }
 
-  login = () => {
-    // return __awaiter(this, void 0, void 0, function () {
-    //     var _a, cogIdpForInitAuthOnly, username, password, init, logins, creds, iot;
-    //     return __generator(this, function (_b) {
-    //         switch (_b.label) {
-    //             case 0:
-    //                 cogIdpForInitAuthOnly = new AWS.CognitoIdentityServiceProvider({
-    //                     // Need mock creds ...
-    //                     credentials: {
-    //                         secretAccessKey: '_',
-    //                         accessKeyId: '_'
-    //                     },
-    //                     region: this.options.awsResources.region
-    //                 });
-    //                 logging_1.dbg('Logging in', this.options.userCredentials);
-    //                 // TODO: won't ever be null if a default is set
-    //                 if (this.options.userCredentials == null) {
-    //                     return [2 /*return*/];
-    //                 }
-    //                 username = this.options.userCredentials.user;
-    //                 password = this.options.userCredentials.password;
-    //                 // TODO: Actually create a `demo` account in the cloud formation ?
-    //                 if (username === 'demo') {
-    //                     return [2 /*return*/];
-    //                 }
-    //                 return [4 /*yield*/, cogIdpForInitAuthOnly.initiateAuth({
-    //                         ClientId: this.options.awsResources.userPoolClient,
-    //                         AuthFlow: 'USER_PASSWORD_AUTH',
-    //                         AuthParameters: { USERNAME: username, PASSWORD: password }
-    //                     }).promise()
-    //                     // TODO: report this, store refresh token
-    //                     // assume this was to try and tell TypeScript that the object was not null
-    //                     // underneath
-    //                 ];
-    //             case 1:
-    //                 init = _b.sent();
-    //                 // TODO: report this, store refresh token
-    //                 // assume this was to try and tell TypeScript that the object was not null
-    //                 // underneath
-    //                 if (init.AuthenticationResult == null) {
-    //                     return [2 /*return*/];
-    //                 }
-    //                 logins = (_a = {},
-    //                     _a['cognito-idp.' +
-    //                         this.options.awsResources.region +
-    //                         '.amazonaws.com/' + this.options.awsResources.userPool] = init.AuthenticationResult.IdToken,
-    //                     _a);
-    //                 creds = new AWS.CognitoIdentityCredentials({
-    //                     Logins: logins,
-    //                     IdentityPoolId: this.options.awsResources.identityPool
-    //                 });
-    //                 // TODO: schedule refresh on all of this
-    //                 return [4 /*yield*/, util_1.promisify(creds.refresh.bind(creds))()];
-    //             case 2:
-    //                 // TODO: schedule refresh on all of this
-    //                 _b.sent();
-    //                 iot = new AWS.Iot({
-    //                     region: this.options.awsResources.region,
-    //                     credentials: creds
-    //                 });
-    //                 return [4 /*yield*/, iot.attachPrincipalPolicy({
-    //                         policyName: this.options.awsResources.iotOwnThingsPolicy,
-    //                         principal: creds.identityId
-    //                     }).promise()
-    //                     // this is an async method, which uses waitCredentials
-    //                 ];
-    //             case 3:
-    //                 _b.sent();
-    //                 // this is an async method, which uses waitCredentials
-    //                 this.testCredentials();
-    //                 // TODO: what about failing ?
-    //                 // this.credentialsSubject.error()
-    //                 this.credentialsSubject.next(creds);
-    //                 logging_1.dbg('Logged in');
-    //                 return [2 /*return*/];
-    //         }
-    //     });
-    // });
+  login = async () => {
+    const cogIdpForInitAuthOnly = new CognitoIdentityServiceProvider({
+      // Need mock creds ...
+      credentials: {
+        secretAccessKey: "_",
+        accessKeyId: "_"
+      },
+      region: this.options.awsResources.region
+    })
+
+    logging_1.dbg("Logging in", this.options.userCredentials)
+
+    // TODO: won't ever be null if a default is set
+    if (this.options.userCredentials == null) {
+      return
+    }
+
+    const username = this.options.userCredentials.user
+    const password = this.options.userCredentials.password
+
+    // TODO: Actually create a `demo` account in the cloud formation ?
+    if (username === "demo") {
+      return
+    }
+
+    const init = await cogIdpForInitAuthOnly
+      .initiateAuth({
+        ClientId: this.options.awsResources.userPoolClient,
+        AuthFlow: "USER_PASSWORD_AUTH",
+        AuthParameters: { USERNAME: username, PASSWORD: password }
+      })
+      .promise()
+    // TODO: report this, store refresh token
+    // assume this was to try and tell TypeScript that the object was not null
+    // underneath
+
+    if (init.AuthenticationResult == null) {
+      return
+    }
+
+    const federation =
+      "cognito-idp." +
+      this.options.awsResources.region +
+      ".amazonaws.com/" +
+      this.options.awsResources.userPool
+
+    const creds = new CognitoIdentityCredentials({
+      Logins: {
+        [federation]: init.AuthenticationResult.IdToken
+      },
+      IdentityPoolId: this.options.awsResources.identityPool
+    })
+    // TODO: schedule refresh on all of this
+
+    await promisify(creds.refresh.bind(creds))()
+
+    // TODO: schedule refresh on all of this
+    const iot = new Iot({
+      region: this.options.awsResources.region,
+      credentials: creds
+    })
+
+    await iot
+      .attachPrincipalPolicy({
+        policyName: this.options.awsResources.iotOwnThingsPolicy,
+        principal: creds.identityId
+      })
+      .promise()
+    // this is an async method, which uses waitCredentials
+
+    // this is an async method, which uses waitCredentials
+    this.testCredentials()
+    // TODO: what about failing ?
+
+    // this.credentialsSubject.error()
+    this.credentialsSubject.next(creds)
+
+    logging_1.dbg("Logged in")
   }
 
   /**
