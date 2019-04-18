ae6bf6ee01b70e767c59497ed3bc39894339db70
Matthew Keil <malibuss67@gmail.com>
2019-04-18 12:40:53 -0400

    aws resources complete

--- src/app/config/aws-resources-test.ts
+++ src/app/config/aws-resources-test.ts
@@ -1,11 +1,11 @@
-export const awsResources = {
+export const awsResourcesTest = {
   region: 'us-east-1',
   stackName: 'pass-ninja-web-test',
-  UserPool: 'us-east-1_uxobyz49W',
-  ApiGatewayRestApi: '5my1p1ij68',
+  UserPool: 'us-east-1_A1YVKkzgz',
+  ApiGatewayRestApi: 'v9b4qnu6c7',
   DemoPassBucket: 'pass-ninja-web-test-us-east-1-demo-passes-bucket',
-  UserPoolClient: '2pd7idnl2ibiudjufeifrea50a',
-  IdentityPool: 'us-east-1:4f7a8891-b52d-4a16-8d51-db93ef3e0be5',
+  UserPoolClient: '34q0slnfght2jmq8u6icmal9b1',
+  IdentityPool: 'us-east-1:04c85d54-3d90-4b87-ad15-540110450588',
   iotOwnThingsPolicy: 'pass-ninja-web-test-us-east-1-cognitos-own',
   iotThingsOwnPolicy: 'pass-ninja-web-test-us-east-1-things-own'
 };
