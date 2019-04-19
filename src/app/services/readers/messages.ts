5e589e87ea041c5ef6150690892be83886c041e7
Matthew Keil <malibuss67@gmail.com>
2019-04-19 16:37:14 -0400

    messages complete

--- src/app/services/readers/messages.ts
+++ src/app/services/readers/messages.ts
@@ -1,22 +1,16 @@
-
-Object.defineProperty(exports, \"__esModule\", { value: true });
-var CommandKey;
-(function (CommandKey) {
-    CommandKey[CommandKey[\"select_ose\"] = 1] = \"select_ose\";
-    CommandKey[CommandKey[\"negotiate_session\"] = 2] = \"negotiate_session\";
-    CommandKey[CommandKey[\"get_smart_tap_data\"] = 3] = \"get_smart_tap_data\";
-    CommandKey[CommandKey[\"get_more_smart_tap_data\"] = 4] = \"get_more_smart_tap_data\";
-    CommandKey[CommandKey[\"received_response\"] = 5] = \"received_response\";
-    CommandKey[CommandKey[\"send_apdu\"] = 6] = \"send_apdu\";
-    CommandKey[CommandKey[\"got_data\"] = 7] = \"got_data\";
-    CommandKey[CommandKey[\"get_data\"] = 8] = \"get_data\";
-    CommandKey[CommandKey[\"un_power\"] = 9] = \"un_power\";
-    CommandKey[CommandKey[\"decrypt_smart_tap_data\"] = 10] = \"decrypt_smart_tap_data\";
-    CommandKey[CommandKey[\"decrypted_smart_tap_data\"] = 11] = \"decrypted_smart_tap_data\";
-    CommandKey[CommandKey[\"get_vas_data\"] = 12] = \"get_vas_data\";
-    CommandKey[CommandKey[\"decrypt_vas_data\"] = 13] = \"decrypt_vas_data\";
-    CommandKey[CommandKey[\"decrypted_vas_data\"] = 14] = \"decrypted_vas_data\";
-})(CommandKey = exports.CommandKey || (exports.CommandKey = {}));
-
-
-//# sourceURL=webpack://commonjs/./src/services/readers/messages.ts?"
+export const enum CommandKey {
+  select_ose,
+  negotiate_session,
+  get_smart_tap_data,
+  get_more_smart_tap_data,
+  received_response,
+  send_apdu,
+  got_data,
+  get_data,
+  un_power,
+  decrypt_smart_tap_data,
+  decrypted_smart_tap_data,
+  get_vas_data,
+  decrypt_vas_data,
+  decrypted_vas_data
+}
