a4fd79ee1720f4a30238a28e9d59c34837807f3d
Matthew Keil <malibuss67@gmail.com>
2019-04-21 18:25:18 -0400

    cli auth service complete

---
+++ lib/AuthorizationService.ts
@@ -0,0 +1,69 @@
+import {
+  CognitoIdentityServiceProvider,
+  CognitoIdentity,
+  CognitoIdentityCredentials
+} from 'aws-sdk'
+import { BehaviorSubject } from 'rxjs'
+import { PassNinjaCliOptions } from './options'
+
+export class AuthorizationService {
+  get credentials() {
+    return this._$credentials.getValue()
+  }
+
+  get $credentials() {
+    return this._$credentials.asObservable()
+  }
+
+  private _provider = new CognitoIdentityServiceProvider({
+    region: this.options.resources.region
+  })
+
+  private _credentials: CognitoIdentityCredentials
+
+  private _$credentials = new BehaviorSubject<CognitoIdentity.Credentials>(
+    {} as any
+  )
+
+  constructor(private options: PassNinjaCliOptions) {
+    this.setup().then(() => console.log('logged in'))
+  }
+
+  update = async () => {
+    await this._credentials.refreshPromise()
+    this._$credentials.next(this._credentials.data.Credentials)
+    return this.credentials
+  }
+
+  private setup = async () => {
+    const response = await this._provider
+      .initiateAuth({
+        ClientId: this.options.resources.userPoolClientId,
+        AuthFlow: 'USER_PASSWORD_AUTH',
+        AuthParameters: {
+          USERNAME: this.options.username,
+          PASSWORD: this.options.password
+        }
+      })
+      .promise()
+
+    if (!response.AuthenticationResult) {
+      throw new Error(
+        'could not login to authentication provider. check username and password'
+      )
+    }
+
+    this._credentials = new CognitoIdentityCredentials({
+      IdentityPoolId: this.options.resources.identityPoolId,
+      Logins: {
+        [this.options.resources.federation]:
+          response.AuthenticationResult.IdToken
+      }
+    })
+
+    // not sure why this was in the original code or if it was dewebpacked correctly
+    // await this.credentials.refreshPromise()
+
+    this._$credentials.next(this._credentials.data.Credentials)
+  }
+}
