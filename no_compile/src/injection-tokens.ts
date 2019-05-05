f776071b82ae0dab259d46210cb0b264dd1f6bbb
Matthew Keil <malibuss67@gmail.com>
2019-05-05 13:08:12 -0500

    finished local-session-handler.service.ts

---
+++ no_compile/src/injection-tokens.ts
@@ -0,0 +1,4 @@
+import { InjectionToken } from "./node_modules/@angular/core";
+
+export const CONFIG_TOKEN = new InjectionToken("App options");
+export const EXPRESS_TOKEN = new InjectionToken("Express app");
