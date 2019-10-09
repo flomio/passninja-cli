3b30514379e085bb8af70d588c6cee1552c72007
Blair Ellis <blair@1-ab.com>
2019-10-09 11:34:19 -0400

    fixed config

--- lib/ConfigurationService.ts
+++ lib/ConfigurationService.ts
@@ -2,6 +2,8 @@ import * as os from 'os';
 import * as fs from 'fs';
 import * as path from 'path';
 
+require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
+
 declare interface NfcKey {
   privateKeyPem: string;
 }
