4d9b91eb888b8cbbc788aaaceb94844ee8bac05a
Richard Grundy <richard@flomio.com>
2021-04-22 17:35:08 -0400

    quick tweak to APDU

--- lib/utils/generateGVD.ts
+++ lib/utils/generateGVD.ts
@@ -13,7 +13,7 @@ export function generateGVD(passTypeIdentifier: string) {
   return (
     '80CA0101369F220201009F2520' +
     createSha256(passTypeIdentifier) +
-    '9F2804C' +
-    '5266B6E9F260400000002'
+    '9F2804' +
+    'C5266B6E9F260400000002'
   );
 }
